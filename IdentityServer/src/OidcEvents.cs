376de70adeb5e3d17c9c274d19883169e6388218
Keagan Gradidge <gradidgek@yahoo.com>
2024-02-15 15:47:52 +0000

    Initial

---
+++ IdentityServer/src/OidcEvents.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Threading.Tasks;
+
+namespace IdentityServerHost
+{
+    public class OidcEvents : OpenIdConnectEvents
+    {
+        public override Task AuthorizationCodeReceived(AuthorizationCodeReceivedContext context)
+        {
+            return base.AuthorizationCodeReceived(context);
+        }
+    }
+}
